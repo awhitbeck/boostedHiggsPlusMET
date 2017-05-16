f9c1b373432ab0ba038b267af4594f2fd1d1a830
awhitbeck <whitbeck.andrew@gmail.com>
2017-05-16 08:09:31 -0500

    changing binning definition

--- src/ALPHABET.h
+++ src/ALPHABET.h
@@ -1,7 +1,11 @@
 namespace alphabet{
 
-const int numMETbins=4;
-const int lowestMET=100.;
-const int binWidth=200.;
+    const int numMETbins=4;
+    const int lowestMET=100.;
+    const int binWidth=200.;
+
+    //const int numMETbins=3;
+    //const int lowestMET=300.;
+    //const int binWidth=200.;
 
 }
