478382aba74591f4e2555d0c3dc77a8b25314d59
awhitbeck <whitbeck.andrew@gmail.com>
2017-05-16 08:14:35 -0500

    tweaking hardcoded paths

--- src/makeTar.sh
+++ src/makeTar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $CMSSW_BASE/../
-tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='*root' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor'
+tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='CMSSW_7_4_2/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='CMSSW_7_4_2/*/bin/*'
 mv workingArea.tar CMSSW_7_4_2/src/boostedHiggsPlusMET/src/.
