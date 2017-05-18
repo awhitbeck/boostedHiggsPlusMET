e087fc44d3aadd4ce54dc48bc31f2ba88d8266fe
awhitbeck <whitbeck.andrew@gmail.com>
2017-05-18 18:13:22 -0500

    bug fix

--- src/submitBatch.sh
+++ src/submitBatch.sh
@@ -5,8 +5,10 @@ branch=$1
 hash=`git rev-parse origin/${branch}`
 
 if [ ! -e /eos/uscms/store/user/${USER}/boostedHiggsPlusMET/ ] 
+    then
     echo "I need /eos/uscms/store/user/${USER}/boostedHiggsPlusMET/"
     exit
+fi
 
 echo "hash ${hash}"               # this is intended to correspond to the hash of your git commit for tracking/reproducibility
                                   # -- you can get the hash with `git rev-parse origin/RA2b_V12_v0`
