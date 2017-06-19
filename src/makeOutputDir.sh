93e9598bccb0c2e0a91d6bdcaac27a2f107a141d
awhitbeck <whitbeck.andrew@gmail.com>
2017-06-19 17:20:36 -0500

    adding output dir for loose photon VR plotObs plots.

--- src/makeOutputDir.sh
+++ src/makeOutputDir.sh
@@ -10,7 +10,7 @@ fi
 
 mkdir /eos/uscms/store/user/${USER}/boostedHiggsPlusMET/$newDir
 mkdir /eos/uscms/store/user/${USER}/boostedHiggsPlusMET/$newDir/plots
-plotsDirList="plotObs_singleEleCR_baseline_plots plotObs_doubleHiggsTag_plots plotObs_singleHiggsTag_plots plotObs_singleMuCR_baseline_plots ABCDscaleFactors cutFlow_plots cutFlow_lowDPhi_plots cutFlow_singleEleCR_plots cutFlow_singleMuCR_plots plotObs_lowDPhi_baseline_plots ALPHABET NminusOne_plots plotObs_antitag_plots plotObs_photon_baseline_plots plotObs_baseline_plots"
+plotsDirList="plotObs_singleEleCR_baseline_plots plotObs_doubleHiggsTag_plots plotObs_singleHiggsTag_plots plotObs_singleMuCR_baseline_plots ABCDscaleFactors cutFlow_plots cutFlow_lowDPhi_plots cutFlow_singleEleCR_plots cutFlow_singleMuCR_plots plotObs_lowDPhi_baseline_plots ALPHABET NminusOne_plots plotObs_antitag_plots plotObs_photon_baseline_plots plotObs_photon_looseCuts_baseline_plots plotObs_baseline_plots"
 for i in $plotsDirList 
 do 
     mkdir /eos/uscms/store/user/${USER}/boostedHiggsPlusMET/$newDir/plots/$i
