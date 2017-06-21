9fac030b98a8c43cb75843b3ff34969a298b5cca
awhitbeck <whitbeck.andrew@gmail.com>
2017-06-21 14:34:22 -0500

    some minor fixes to exec_name.  Adding loose ALPHABET_photon job.  Adding loose photon plotObs job.

--- src/submitBatch.sh
+++ src/submitBatch.sh
@@ -19,7 +19,7 @@ cd $submitDir
 condor_submit batchExecute.jdl exec_name="cutFlow_signal" arguments="${hash} cutFlow 0"
 condor_submit batchExecute.jdl exec_name="cutFlow_singleMu" arguments="${hash} cutFlow 1"
 condor_submit batchExecute.jdl exec_name="cutFlow_singleEle" arguments="${hash} cutFlow 2"
-condor_submit batchExecute.jdl exec_name="cutFlow_photon" arguments="${hash} cutFlow 3"
+condor_submit batchExecute.jdl exec_name="cutFlow_lowDphi" arguments="${hash} cutFlow 3"
 
 condor_submit batchExecute.jdl exec_name="plotObs_baseline" arguments="${hash} plotObs_baseline"
 condor_submit batchExecute.jdl exec_name="plotObs_antitag" arguments="${hash} plotObs_antitag"
@@ -30,6 +30,7 @@ condor_submit batchExecute.jdl exec_name="plotObs_singleEleCR_baseline" argument
 condor_submit batchExecute.jdl exec_name="plotObs_singleMuCR_baseline" arguments="${hash} plotObs_singleMuCR_baseline"
 
 condor_submit batchExecute.jdl exec_name="plotObs_photon_baseline" arguments="${hash} plotObs_photon_baseline"
+condor_submit batchExecute.jdl exec_name="plotObs_photon_looseCuts_baseline" arguments="${hash} plotObs_photon_baseline 1"
 
 condor_submit batchExecute.jdl exec_name="plotObs_lowDPhi_baseline" arguments="${hash} plotObs_lowDPhi_baseline"
 
@@ -44,6 +45,7 @@ condor_submit batchExecute.jdl exec_name="ALPHABET_singleEle_loose" arguments="$
 condor_submit batchExecute.jdl exec_name="ALPHABET_lowDPhi_loose" arguments="${hash} ALPHABET 3 1"
 
 condor_submit batchExecute.jdl exec_name="ALPHABET_photon" arguments="${hash} ALPHABET_photon"
+condor_submit batchExecute.jdl exec_name="ALPHABET_photon_loose" arguments="${hash} ALPHABET_photon 1"
 
 condor_submit batchExecute.jdl exec_name="datacardInputs" arguments="${hash} datacardInputs"
 
