c37b4471ab2144ec3e249aefc00f05acf1115000
awhitbeck <whitbeck.andrew@gmail.com>
2017-07-14 08:34:53 -0500

    add dumps of scale factors for logging, add configurability for integrating from MET>100 or MET>300, etc.

--- src/checkScaleFactors.cc
+++ src/checkScaleFactors.cc
@@ -1,37 +1,39 @@
 #include "CMS_lumi.cc"
 #include "computeRatio.h"
 
-void computeIntegratedRatio(TH1F* num, TH1F* denom){
+void computeIntegratedRatio(TH1F* num, TH1F* denom, int start_bin=1, int end_bin=4,bool verbose=false){
     
     TH1F* num_int = new TH1F("num_int","num_int",1,100,900);
     TH1F* denom_int = new TH1F("denom_int","denom_int",1,100,900);
     double error;
 
-    num_int->SetBinContent(1,num->IntegralAndError(1,4,error));
+    num_int->SetBinContent(1,num->IntegralAndError(start_bin,end_bin,error));
     num_int->SetBinError(1,error);
 
-    denom_int->SetBinContent(1,denom->IntegralAndError(1,4,error));
+    denom_int->SetBinContent(1,denom->IntegralAndError(start_bin,end_bin,error));
     denom_int->SetBinError(1,error);
 
     TGraphAsymmErrors graph;
-    SetRatioErr(num_int,denom_int,graph,1,100.,1000.);
+    SetRatioErr(num_int,denom_int,graph,1,100.,1000.,verbose);
 
     delete num_int;
     delete denom_int;
 }
 
-void computeScaleFactor(TH1F* hdata,TH1F* hmc){
+void computeScaleFactor(TH1F* hdata,TH1F* hmc, bool verbose=false){
     double yieldmc,yeilddata,errmc,errdata;
 
     //cout << "hdata: " << hdata << endl;
     //cout << "hmc: " << hmc << endl;
 
-    yieldmc = hmc->IntegralAndError(1,3,errmc) ; 
-    yielddata = hdata->IntegralAndError(1,3,errdata) ;
-    if( yielddata == 0. )
-        cout << " 0. +/- " <<  sqrt(errmc*errmc+1.67*1.67);
-    else 
-        cout << yielddata/yieldmc << " +/- " << sqrt(errmc*errmc/yieldmc/yieldmc+errdata*errdata/yielddata/yielddata)*yielddata/yieldmc;
+    yieldmc = hmc->IntegralAndError(1,4,errmc) ; 
+    yielddata = hdata->IntegralAndError(1,4,errdata) ;
+    if( verbose ){
+        if( yielddata == 0. )
+            cout << " 0. +/- " <<  sqrt(errmc*errmc+1.67*1.67);
+        else 
+            cout << yielddata/yieldmc << " +/- " << sqrt(errmc*errmc/yieldmc/yieldmc+errdata*errdata/yielddata/yielddata)*yielddata/yieldmc;
+    }
 }
 
 void setStyle(TGraphAsymmErrors& graph, TString ylabel="Data/MC"){
@@ -96,7 +98,7 @@ TLine* makeRatioLine(TH1F* num , TH1F* denom, double lower, double upper=900.){
     return line;
 }
 
-void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString baseDir="./"){
+void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString baseDir="./",int integrate_from=1,int integrate_to=4){
 
     gROOT->ProcessLine(".L tdrstyle.C");
     gROOT->ProcessLine("setTDRStyle()");
@@ -145,161 +147,161 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
     prediction->SetMarkerColor(4);
     prediction->SetLineColor(4);
 
-    cout << "\\begin{tabular}{|c|c|}" << endl;
+    //cout << "\\begin{tabular}{|c|c|}" << endl;
 
     // ---------------------------------------------
-    cout << "\\multicolumn{2}{c}{100 $<$ MET $<$ 300} \\\\ \\hline" << endl;
+    //cout << "\\multicolumn{2}{c}{100 $<$ MET $<$ 300} \\\\ \\hline" << endl;
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SR_100_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SR_100_sum");
-    cout << std::setprecision(2) << "$\\alpha_{}$ & ";
+    //cout << std::setprecision(2) << "$\\alpha_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSRdata->SetBinContent(1,hdata->Integral(1,3));
     doubletagSRmc->SetBinContent(1,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SB_100_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SB_100_sum");
-    cout << "$\\beta_{}$ &";
+    //cout << "$\\beta_{}$ &";
     computeScaleFactor(hdata,hmc);
     doubletagSBdata->SetBinContent(1,hdata->Integral(1,3));
     doubletagSBmc->SetBinContent(1,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSR_100_data");
     hmc = (TH1F*) f->Get("mJ_antitagSR_100_sum");
-    cout << "$\\gamma_{}$ & ";
+    //cout << "$\\gamma_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSRdata->SetBinContent(1,hdata->Integral(1,3));
     antitagSRmc->SetBinContent(1,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSB_100_data");
     hmc = (TH1F*) f->Get("mJ_antitagSB_100_sum");
-    cout << "$\\delta_{}$ & ";
+    //cout << "$\\delta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSBdata->SetBinContent(1,hdata->Integral(1,3));
     antitagSBmc->SetBinContent(1,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     // ---------------------------------------------
-    cout << "\\multicolumn{2}{c}{300 $<$ MET $<$ 500} \\\\ \\hline" << endl;
+    //cout << "\\multicolumn{2}{c}{300 $<$ MET $<$ 500} \\\\ \\hline" << endl;
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SR_300_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SR_300_sum");
-    cout << "$\\alpha_{}$ & ";
+    //cout << "$\\alpha_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSRdata->SetBinContent(2,hdata->Integral(1,3));
     doubletagSRmc->SetBinContent(2,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SB_300_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SB_300_sum");
-    cout << "$\\beta_{}$ & ";
+    //cout << "$\\beta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSBdata->SetBinContent(2,hdata->Integral(1,3));
     doubletagSBmc->SetBinContent(2,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSR_300_data");
     hmc = (TH1F*) f->Get("mJ_antitagSR_300_sum");
-    cout << "$\\gamma_{}$ & ";
+    //cout << "$\\gamma_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSRdata->SetBinContent(2,hdata->Integral(1,3));
     antitagSRmc->SetBinContent(2,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSB_300_data");
     hmc = (TH1F*) f->Get("mJ_antitagSB_300_sum");
-    cout << "$\\delta_{}$ & ";
+    //cout << "$\\delta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSBdata->SetBinContent(2,hdata->Integral(1,3));
     antitagSBmc->SetBinContent(2,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
     
     // ---------------------------------------------
-    cout << "\\multicolumn{2}{c}{500 $<$ MET $<$ 700} \\\\ \\hline" << endl;
+    //cout << "\\multicolumn{2}{c}{500 $<$ MET $<$ 700} \\\\ \\hline" << endl;
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SR_500_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SR_500_sum");
-    cout << "$\\alpha_{}$ & ";
+    //cout << "$\\alpha_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSRdata->SetBinContent(3,hdata->Integral(1,3));
     doubletagSRmc->SetBinContent(3,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SB_500_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SB_500_sum");
-    cout << "$\\beta_{}$ & ";
+    //cout << "$\\beta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSBdata->SetBinContent(3,hdata->Integral(1,3));
     doubletagSBmc->SetBinContent(3,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSR_500_data");
     hmc = (TH1F*) f->Get("mJ_antitagSR_500_sum");
-    cout << "$\\gamma_{}$ & ";
+    //cout << "$\\gamma_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSRdata->SetBinContent(3,hdata->Integral(1,3));
     antitagSRmc->SetBinContent(3,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSB_500_data");
     hmc = (TH1F*) f->Get("mJ_antitagSB_500_sum");
-    cout << "$\\delta_{}$ & ";
+    //cout << "$\\delta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSBdata->SetBinContent(3,hdata->Integral(1,3));
     antitagSBmc->SetBinContent(3,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     // ---------------------------------------------
-    cout << "\\multicolumn{2}{c}{MET $>$ 700} \\\\ \\hline" << endl;
+    //cout << "\\multicolumn{2}{c}{MET $>$ 700} \\\\ \\hline" << endl;
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SR_700_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SR_700_sum");
-    cout << "$\\alpha_{}$ & ";
+    //cout << "$\\alpha_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSRdata->SetBinContent(4,hdata->Integral(1,3));
     doubletagSRmc->SetBinContent(4,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_"+tagString+"SB_700_data");
     hmc = (TH1F*) f->Get("mJ_"+tagString+"SB_700_sum");
-    cout << "$\\beta_{}$ & ";
+    //cout << "$\\beta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     doubletagSBdata->SetBinContent(4,hdata->Integral(1,3));
     doubletagSBmc->SetBinContent(4,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSR_700_data");
     hmc = (TH1F*) f->Get("mJ_antitagSR_700_sum");
-    cout << "$\\gamma_{}$ & ";
+    //cout << "$\\gamma_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSRdata->SetBinContent(4,hdata->Integral(1,3));
     antitagSRmc->SetBinContent(4,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
     hdata = (TH1F*) f->Get("mJ_antitagSB_700_data");
     hmc = (TH1F*) f->Get("mJ_antitagSB_700_sum");
-    cout << "$\\delta_{}$ & ";
+    //cout << "$\\delta_{}$ & ";
     computeScaleFactor(hdata,hmc);
     antitagSBdata->SetBinContent(4,hdata->Integral(1,3));
     antitagSBmc->SetBinContent(4,hmc->Integral(1,3));
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
     // ---------------------------------------------
-    cout << "\\multicolumn{2}{c}{All MET} \\\\ \\hline" << endl;
-    cout << "$\\alpha_{}$ & ";
+    //cout << "\\multicolumn{2}{c}{All MET} \\\\ \\hline" << endl;
+    //cout << "$\\alpha_{}$ & ";
     computeScaleFactor(doubletagSRdata,doubletagSRmc);
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
-    cout << "$\\beta_{}$ & ";
+    //cout << "$\\beta_{}$ & ";
     computeScaleFactor(doubletagSBdata,doubletagSBmc);
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
 
-    cout << "$\\gamma_{}$ & ";
+    //cout << "$\\gamma_{}$ & ";
     computeScaleFactor(antitagSRdata,antitagSRmc);
-    cout << " \\\\ \\hline" << endl;
+    //cout << " \\\\ \\hline" << endl;
     
-    cout << "$\\delta_{}$ & ";
+    //cout << "$\\delta_{}$ & ";
     computeScaleFactor(antitagSBdata,antitagSBmc);
-    cout << " \\\\ \\hline" << endl;
-    cout << "\\end{tabular}" << endl;
+    //cout << " \\\\ \\hline" << endl;
+    //cout << "\\end{tabular}" << endl;
     // ---------------------------------------------
     // ---------------------------------------------
     for( int i = 1 ; i <= doubletagSRmc->GetNbinsX() ; i++ ){        
@@ -353,7 +355,11 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
     botPad->cd();
 
     TGraphAsymmErrors MCRatio;
-    SetRatioErr(doubletagSRdata,doubletagSRmc,MCRatio);
+    if( doubletag )
+        cout << "doubletagSR:" << endl;
+    else
+        cout << "tagSR:" << endl;
+    SetRatioErr(doubletagSRdata,doubletagSRmc,MCRatio,4,100.,900.,true);
     setStyle(MCRatio);
     if( tag == "_lowDphi" || tag == "_looseCuts_lowDphi" )
         MCRatio.GetXaxis()->SetLimits(300.,900.);
@@ -423,8 +429,12 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
     can->GetFrame()->Draw();
 
     botPad->cd();
-
-    SetRatioErr(doubletagSBdata,doubletagSBmc,MCRatio);
+    
+    if( doubletag )
+        cout << "doubletagSB: " << endl;
+    else
+        cout << "tagSB: " << endl;
+    SetRatioErr(doubletagSBdata,doubletagSBmc,MCRatio,4,100.,900.,true);
     setStyle(MCRatio);
     if( tag == "_lowDphi" || tag == "_looseCuts_lowDphi" )
         MCRatio.GetXaxis()->SetLimits(300.,900.);
@@ -457,7 +467,8 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
 
     botPad->cd();
 
-    SetRatioErr(antitagSRdata,antitagSRmc,MCRatio);
+    cout << "antitagSR:" << endl;
+    SetRatioErr(antitagSRdata,antitagSRmc,MCRatio,4,100.,900.,true);
     setStyle(MCRatio);
     if( tag == "_lowDphi" || tag == "_looseCuts_lowDphi" )
         MCRatio.GetXaxis()->SetLimits(300.,900.);
@@ -490,7 +501,8 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
 
     botPad->cd();
 
-    SetRatioErr(antitagSBdata,antitagSBmc,MCRatio);
+    cout << "antitagSB: " << endl;
+    SetRatioErr(antitagSBdata,antitagSBmc,MCRatio,4,100.,900.,true);
     setStyle(MCRatio);
     if( tag == "_lowDphi" || tag == "_looseCuts_lowDphi" )
         MCRatio.GetXaxis()->SetLimits(300.,900.);
@@ -507,25 +519,25 @@ void checkScaleFactors(TString tag = "_singleMu", bool doubletag = true, TString
     cout << "// Integrated stats " << endl;
     cout << "// - - - - - - - - - - - - - - - - - - - - " << endl;
     cout << "antitag SR: " << endl;
-    computeIntegratedRatio(antitagSRdata,antitagSRmc);
+    computeIntegratedRatio(antitagSRdata,antitagSRmc,integrate_from,integrate_to,true);
     cout << "antitag SB: " << endl;
-    computeIntegratedRatio(antitagSBdata,antitagSBmc);
+    computeIntegratedRatio(antitagSBdata,antitagSBmc,integrate_from,integrate_to,true);
     if(doubletag)
         cout << "doubletag SR: " << endl;
     else
         cout << "tag SR: " << endl;
-    computeIntegratedRatio(doubletagSRdata,doubletagSRmc);
+    computeIntegratedRatio(doubletagSRdata,doubletagSRmc,integrate_from,integrate_to,true);
     if(doubletag)
         cout << "doubletag SB: " << endl;
     else 
         cout << "tag SB: " << endl;
-    computeIntegratedRatio(doubletagSBdata,doubletagSBmc);
+    computeIntegratedRatio(doubletagSBdata,doubletagSBmc,integrate_from,integrate_to,true);
 
     if(doubletag)
         cout << "closure 2H: " << endl;
     else
         cout << "closure 2H: " << endl;
-    computeIntegratedRatio(doubletagSRdata,prediction);
+    computeIntegratedRatio(doubletagSRdata,prediction,integrate_from,integrate_to,true);
 
 }
 
