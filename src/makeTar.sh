5e113148e91738eaf900dce14aedcf1a8e431246
awhitbeck <whitbeck.andrew@gmail.com>
2017-08-22 15:14:16 -0500

    optimizing contents of tar

--- src/makeTar.sh
+++ src/makeTar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd $CMSSW_BASE/../
-tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='CMSSW_7_4_2/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='CMSSW_7_4_2/*/bin/*'
+tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='CMSSW_7_4_2/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='CMSSW_7_4_2/*/bin/*' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/.git/*' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/src/*dag' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/datacards'
 mv workingArea.tar CMSSW_7_4_2/src/boostedHiggsPlusMET/src/.
