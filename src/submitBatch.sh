a880b0241bfebb0211cf1ed30e9ad2e4df325a3e
awhitbeck <whitbeck.andrew@gmail.com>
2018-10-24 15:02:14 -0500

    tweak exec_name so that I can see the stdout and stderr for each job

--- src/submitBatch.sh
+++ src/submitBatch.sh
@@ -16,38 +16,41 @@ source makeOutputDir.sh $hash     # setup directories in eos for output
 source makeTar.sh                 # package up code to transfer to worker nodes
 cd $submitDir
 
-condor_submit batchExecute.jdl exec_name="cutFlow_signal" arguments="${hash} cutFlow 0"
-condor_submit batchExecute.jdl exec_name="cutFlow_singleMu" arguments="${hash} cutFlow 1"
-condor_submit batchExecute.jdl exec_name="cutFlow_singleEle" arguments="${hash} cutFlow 2"
-condor_submit batchExecute.jdl exec_name="cutFlow_lowDphi" arguments="${hash} cutFlow 3"
+# condor_submit batchExecute.jdl exec_name="cutFlow_signal" arguments="${hash} cutFlow 0"
+# condor_submit batchExecute.jdl exec_name="cutFlow_singleMu" arguments="${hash} cutFlow 1"
+# condor_submit batchExecute.jdl exec_name="cutFlow_singleEle" arguments="${hash} cutFlow 2"
+# condor_submit batchExecute.jdl exec_name="cutFlow_lowDphi" arguments="${hash} cutFlow 3"
 
-condor_submit batchExecute.jdl exec_name="plotObs_baseline" arguments="${hash} plotObs_baseline"
-condor_submit batchExecute.jdl exec_name="plotObs_antitag" arguments="${hash} plotObs_antitag"
-condor_submit batchExecute.jdl exec_name="plotObs_doubleHiggsTag" arguments="${hash} plotObs_doubleHiggsTag"
-condor_submit batchExecute.jdl exec_name="plotObs_singleHiggsTag" arguments="${hash} plotObs_singleHiggsTag"
+condor_submit batchExecute.jdl exec_name="plotObs_baseline" arguments="${hash} plotObs_baseline baseline"
+condor_submit batchExecute.jdl exec_name="plotObs_looseZtag" arguments="${hash} plotObs_baseline looseZtag"
+condor_submit batchExecute.jdl exec_name="plotObs_tightZtag" arguments="${hash} plotObs_baseline tightZtag"
 
-condor_submit batchExecute.jdl exec_name="plotObs_singleEleCR_baseline" arguments="${hash} plotObs_singleEleCR_baseline"
-condor_submit batchExecute.jdl exec_name="plotObs_singleMuCR_baseline" arguments="${hash} plotObs_singleMuCR_baseline"
-condor_submit batchExecute.jdl exec_name="plotObs_singleEleCR_looseCuts_baseline" arguments="${hash} plotObs_singleEleCR_baseline 1"
-condor_submit batchExecute.jdl exec_name="plotObs_singleMuCR_looseCuts_baseline" arguments="${hash} plotObs_singleMuCR_baseline 1"
+# condor_submit batchExecute.jdl exec_name="plotObs_antitag" arguments="${hash} plotObs_antitag"
+# condor_submit batchExecute.jdl exec_name="plotObs_doubleHiggsTag" arguments="${hash} plotObs_doubleHiggsTag"
+# condor_submit batchExecute.jdl exec_name="plotObs_singleHiggsTag" arguments="${hash} plotObs_singleHiggsTag"
 
-condor_submit batchExecute.jdl exec_name="plotObs_photon_baseline" arguments="${hash} plotObs_photon_baseline"
-condor_submit batchExecute.jdl exec_name="plotObs_photon_looseCuts_baseline" arguments="${hash} plotObs_photon_baseline 1"
+# condor_submit batchExecute.jdl exec_name="plotObs_singleEleCR_baseline" arguments="${hash} plotObs_singleEleCR_baseline"
+# condor_submit batchExecute.jdl exec_name="plotObs_singleMuCR_baseline" arguments="${hash} plotObs_singleMuCR_baseline"
+# condor_submit batchExecute.jdl exec_name="plotObs_singleEleCR_looseCuts_baseline" arguments="${hash} plotObs_singleEleCR_baseline 1"
+# condor_submit batchExecute.jdl exec_name="plotObs_singleMuCR_looseCuts_baseline" arguments="${hash} plotObs_singleMuCR_baseline 1"
 
-condor_submit batchExecute.jdl exec_name="plotObs_lowDPhi_baseline" arguments="${hash} plotObs_lowDPhi_baseline"
+# condor_submit batchExecute.jdl exec_name="plotObs_photon_baseline" arguments="${hash} plotObs_photon_baseline"
+# condor_submit batchExecute.jdl exec_name="plotObs_photon_looseCuts_baseline" arguments="${hash} plotObs_photon_baseline 1"
 
-condor_submit batchExecute.jdl exec_name="ALPHABET_signal" arguments="${hash} ALPHABET 0"
-condor_submit batchExecute.jdl exec_name="ALPHABET_singleMu" arguments="${hash} ALPHABET 1"
-condor_submit batchExecute.jdl exec_name="ALPHABET_singleEle" arguments="${hash} ALPHABET 2"
-condor_submit batchExecute.jdl exec_name="ALPHABET_lowDPhi" arguments="${hash} ALPHABET 3"
+# condor_submit batchExecute.jdl exec_name="plotObs_lowDPhi_baseline" arguments="${hash} plotObs_lowDPhi_baseline"
 
-condor_submit batchExecute.jdl exec_name="ALPHABET_signal_loose" arguments="${hash} ALPHABET 0 1"
-condor_submit batchExecute.jdl exec_name="ALPHABET_singleMu_loose" arguments="${hash} ALPHABET 1 1"
-condor_submit batchExecute.jdl exec_name="ALPHABET_singleEle_loose" arguments="${hash} ALPHABET 2 1"
-condor_submit batchExecute.jdl exec_name="ALPHABET_lowDPhi_loose" arguments="${hash} ALPHABET 3 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_signal" arguments="${hash} ALPHABET 0"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_singleMu" arguments="${hash} ALPHABET 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_singleEle" arguments="${hash} ALPHABET 2"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_lowDPhi" arguments="${hash} ALPHABET 3"
 
-condor_submit batchExecute.jdl exec_name="ALPHABET_photon" arguments="${hash} ALPHABET_photon"
-condor_submit batchExecute.jdl exec_name="ALPHABET_photon_loose" arguments="${hash} ALPHABET_photon 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_signal_loose" arguments="${hash} ALPHABET 0 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_singleMu_loose" arguments="${hash} ALPHABET 1 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_singleEle_loose" arguments="${hash} ALPHABET 2 1"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_lowDPhi_loose" arguments="${hash} ALPHABET 3 1"
 
-condor_submit batchExecute.jdl exec_name="datacardInputs" arguments="${hash} datacardInputs"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_photon" arguments="${hash} ALPHABET_photon"
+# condor_submit batchExecute.jdl exec_name="ALPHABET_photon_loose" arguments="${hash} ALPHABET_photon 1"
+
+# condor_submit batchExecute.jdl exec_name="datacardInputs" arguments="${hash} datacardInputs"
 
