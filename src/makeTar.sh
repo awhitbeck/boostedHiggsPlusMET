ed093414de7efd6f88f70c551be2c04790546ac6
awhitbeck <whitbeck.andrew@gmail.com>
2018-10-23 23:36:38 -0500

    generalizing tar script

--- src/makeTar.sh
+++ src/makeTar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CMSSW=`echo $CMSSW_BASE | awk -F '/' '{print $NF}'`
 cd $CMSSW_BASE/../
-tar -cf workingArea.tar CMSSW_7_4_2 --exclude='*.dag.*' --exclude='*tar' --exclude='CMSSW_7_4_2/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='CMSSW_7_4_2/*/bin/*' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/.git/*' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/src/*dag' --exclude='CMSSW_7_4_2/src/boostedHiggsPlusMET/datacards'
-mv workingArea.tar CMSSW_7_4_2/src/boostedHiggsPlusMET/src/.
+tar -cf workingArea.tar $CMSSW --exclude='*.dag.*' --exclude='*tar' --exclude='$CMSSW/*/src/*root' --exclude='*eps' --exclude='*png' --exclude='*pdf' --exclude='*stdout' --exclude='*stderr' --exclude='*condor' --exclude='$CMSSW/*/bin/*' --exclude='$CMSSW/src/boostedHiggsPlusMET/.git/*' --exclude='$CMSSW/src/boostedHiggsPlusMET/src/*dag' --exclude='$CMSSW/src/boostedHiggsPlusMET/datacards'
+mv workingArea.tar $CMSSW/src/boostedHiggsPlusMET/src/.
